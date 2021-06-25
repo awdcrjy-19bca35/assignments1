85103224f4f9f97ca16a60f96f00e1c602f51f37
awdcrjy-19bca35 <86473369+awdcrjy-19bca35@users.noreply.github.com>
2021-06-25 19:56:10 +0530

    Add files via upload

---
+++ hw26jun.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class Main
+{ 
+  public static void main(String str[])
+    { 
+      int n=1;
+      while (n<11)
+      { System.out.println(n+" ");
+        n=-(~n);}
+    } 
+}
