6548829d5f5b92cdbe9fe5b165e03c16afb362b4
awdcrjy-19bca35 <86473369+awdcrjy-19bca35@users.noreply.github.com>
2021-06-25 20:37:36 +0530

    Update hw26jun.java

--- hw26jun.java
+++ hw26jun.java
@@ -4,7 +4,7 @@ class Main
   public static void main(String str[])
     { 
       int n=19;
-      if (x&1!=0)
+      if (n&1!=0)
       { System.out.println("odd");}
       else 
       { System.out.println("even");}
