fd480bcf8d55fb3d1fb3d9695747794de4521116
awdcrjy-19bca35 <86473369+awdcrjy-19bca35@users.noreply.github.com>
2021-06-27 13:49:51 +0530

    Create jun26hw.java

---
+++ jun26hw.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    int year;
+	     boolean leapy;
+	    Scanner p = new Scanner(System.in);
+		System.out.println("Enter year");
+		year=p.nextInt();
+		if(year%4==0)   // year is divided by 4 
+		{
+		    if(year%100==0)  // checking if year is a century
+		    {
+		        if(year%400==0) // checking if the century is a leap year
+		        leapy=true;
+		        else
+		        leapy=false;
+		    }
+		    else
+		    leapy=true; // year is not a century but leap year
+		}else 
+		leapy=false;  // year is not divided by 4 so not a leap year
+	if (leapy)	
+	{System.out.println("Given year is a leap year");}
+	else 
+		{System.out.println("Given year is a leap year");}
+}
+}
