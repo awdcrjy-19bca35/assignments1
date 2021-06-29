a3b08a85aec613af98cf4bce4d302aa95df6c35e
awdcrjy-19bca35 <86473369+awdcrjy-19bca35@users.noreply.github.com>
2021-06-29 21:38:23 +0530

    Create reverseinteger.java

---
+++ reverseinteger.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Reverse {
+         public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int p = Integer.parseInt(bufferedReader.readLine().trim());
+         int r;
+             long rev=0;
+                   
+         if(p>0)
+         {while(p>0)
+        {
+            r=p%10;
+            rev=(rev*10)+r;
+            p=p/10;
+        }
+        System.out.println(""+rev);}
+             else
+             {
+         while(p<0)
+        {
+            r=p%10;
+            rev=(rev*10)+r;
+            p=p/10;
+        }
+        System.out.println(""+rev);}
+             } 
+        
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+// output:
+// 6789
+// 9876
