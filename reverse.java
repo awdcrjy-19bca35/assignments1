a00f04fc111b33223815169f24e8ba2a0be56e39
awdcrjy-19bca35 <86473369+awdcrjy-19bca35@users.noreply.github.com>
2021-06-29 21:27:12 +0530

    Update reverse.java

--- reverse.java
+++ reverse.java
@@ -1,32 +1,29 @@
 
 // java program to print the reverse of the given number // date: 28 june 2021
-
-
-
 import java.io.*;
 import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Reverse {
+public class Solution {
 
-    public static void main(String[] args) {
-            int rev=0,r,p;
-         Scanner s=new Scanner(System.in);
-         System.out.println("Enter your number:");
-         p=s.nextInt();       
-         while(p>0)
+        public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int p = Integer.parseInt(bufferedReader.readLine().trim());
+int r,rev=0;
+        bufferedReader.close();
+            while(p>0)
         {
             r=p%10;
             rev=(rev*10)+r;
             p=p/10;
         }
-        System.out.println("Reverse of input number: "+rev);
+          System.out.println(" "+rev);
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
     }
 }
-
 
 // output:
 // Enter your number:
