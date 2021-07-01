eea9597e9e5027c68bbcc30755aee8098f2d9fcc
awdcrjy-19bca35 <86473369+awdcrjy-19bca35@users.noreply.github.com>
2021-07-01 17:33:34 +0530

    Create pattern1.java

---
+++ pattern1.java
@@ -0,0 +1,35 @@
+// java program to print given pattern // date: 01 july 2021
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    int n;
+	    Scanner sc=new Scanner(System.in);
+	    n=sc.nextInt();
+	    for(int i=n;i>=1;i--)
+	    {
+	        for(int j=1;j<=i;j++)
+	        {
+	          System.out.print("+");  
+	        }
+	        
+	        	System.out.println();
+	      
+	    }  
+	
+	}
+}
+
+//output:
+5
+
++++++
+
+++++
+
++++
+
+++
+
++
