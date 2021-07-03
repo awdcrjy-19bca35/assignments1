f0ab6788dcf045c28873284428bea93764dd4118
awdcrjy-19bca35 <86473369+awdcrjy-19bca35@users.noreply.github.com>
2021-07-03 15:57:44 +0530

    Update pattern2.java

--- pattern2.java
+++ pattern2.java
@@ -16,7 +16,7 @@ public class Main
 	        	System.out.println();
 	      
 	    }
-	    for(int i=n;i>=1;i--)
+	    for(int i=n-1;i>=1;i--)
 	    {
 	        for(int j=1;j<=i;j++)
 	        {
@@ -39,8 +39,6 @@ public class Main
 ***
 
 ****
-
-*****
 
 *****
 
