38f4726514fc81b047726a5e1db844267d2accb1
awdcrjy-19bca35 <86473369+awdcrjy-19bca35@users.noreply.github.com>
2021-07-05 19:51:40 +0530

    Create arraysum.java

---
+++ arraysum.java
@@ -0,0 +1,40 @@
+// program to print sum of array elements // date: 05 july 2021
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    int n,sum=0;
+	    Scanner p=new Scanner(System.in);
+	    System.out.println("Enter array elements:");
+	    n=p.nextInt();
+	    int a[]= new int[n];
+	    for(int i=0;i<n;i++)
+	    {
+	        a[i]=p.nextInt();
+	    }
+	    for(int i=0;i<n;i++)
+	    {
+	        sum+=a[i];
+	    }
+		System.out.println("Sum of array elements:"+sum);
+	}
+}
+/* output:
+Enter array elements:
+
+5
+
+1
+
+2
+
+3
+
+4
+
+5
+
+Sum of array elements:15
+
+
+
