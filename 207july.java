5e219a0386288c2a096d3fef9149721f64c200cd
awdcrjy-19bca35 <86473369+awdcrjy-19bca35@users.noreply.github.com>
2021-07-08 14:34:23 +0530

    Update 207july.java

--- 207july.java
+++ 207july.java
@@ -1,54 +1,48 @@
 // program to print the majority element in the array // date: 07 july 2021
 import java.util.*;
-import java.io.*;
-public class Main
+class Mainmajorele
 {
-	public static void main(String[] args) {
-	    int n;
-	    Scanner s=new Scanner(System.in);
-	    System.out.println("Enter array size and elements:");
-	    n=s.nextInt();
-	    int a[]= new int[n];
-	    for(int i=0;i<n;i++)
-	    {
-	        a[i]=s.nextInt();
-	    }
-	    int c=1,c1=0,t=0;
-	    int nn=n;
-	     for(int i=0;i<n;i++)
-	     {
-	         for(int j=i+1;j<n;)
-	         {
-	             int p=0;
-	             if(a[i]==a[j])
-	             {
-	                 p=j;
-	                 c++;
-	                 for(int k=j;k<n-1;k++)
-	                 {
-	                     a[k]=a[k+1];
-	                 }
-	                 n--;
-	             }
-	            if (a[j]==a[i])
-	            j=p;
-	            else
-	            j++;
-	         }
-	         if(c>nn/2)
-	         {
-	            c1=c;
-	            t=i;
-	            break;
-	         }
-	         c=1;
-	     }
-	     if(c1==0)
-	         System.out.println("No majority element");
-	     else
-	     {
-	     System.out.println("Majority element:"+a[t]);
-	     }
+	public static void main(String args[])
+	{
+	System.out.println("Enter array size and elements:");
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	int a[]=new int[n];
+	for(int i=0;i<n;i++)
+	a[i]=sc.nextInt();
+	int c=1,c1=0,m=0;
+	int n1=n;
+	for(int i=0;i<n;i++)
+	{
+		for(int j=i+1;j<n;)         
+		{  
+			if(a[i]==a[j])
+			{        
+                                c++;
+				for(int k=j;k<n-1;k++)
+				{
+				a[k]=a[k+1];
+				}
+				n--;
+			}
+                      if(a[j]==a[i])
+                      j=j;
+                      else
+                      j++;
+		}
+		if(c>n1/2)
+		{
+		c1=c;
+		m=i;
+		break;
+		}
+                c=1;
+		
+	}
+	if(c1==0)
+	System.out.println("No majority element");
+	else
+        System.out.println(a[m]+" is the majority element");
 	}
 }
 /* ----------------------------------
