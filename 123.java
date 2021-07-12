dac4a8665a60549d17d0d947552111d207022214
awdcrjy-19bca35 <86473369+awdcrjy-19bca35@users.noreply.github.com>
2021-07-12 23:48:56 +0530

    Create 123.java

---
+++ 123.java
@@ -0,0 +1,26 @@
+// java program to capitalise the first letter of each word in a string // date: 12 july 2021
+import java.lang.*;
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner s=new Scanner(System.in);
+	    System.out.println("Enter your string:");
+	    String str=s.nextLine();
+	    String parts[]=str.split(" ");  // using split method 
+      String Str2 ="";               // taking new string 
+        
+        for(String i: parts)         // using for each loop - taking each word of the string
+        {                            // using substring method to seperate the first letter
+            Str2+=i.substring(0,1).toUpperCase()+i.substring(1)+ " ";  
+        }
+        System.out.println(Str2);
+	}
+}
+/*---------------------------------------------------------------
+output:
+Enter your string:
+
+olivia rodrigo sour
+
+Olivia Rodrigo Sour 
