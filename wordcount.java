551144cb4de7d5e927a7149db699be51ca02b887
awdcrjy-19bca35 <86473369+awdcrjy-19bca35@users.noreply.github.com>
2021-07-13 22:23:16 +0530

    Create wordcount.java

---
+++ wordcount.java
@@ -0,0 +1,25 @@
+// java program to count number of words in the given sentence // date: 13 july 2021
+import java.lang.*;
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner s=new Scanner(System.in);
+	    System.out.println("Enter your sentence:");
+	    String str=s.nextLine();
+	    String parts[]=str.split(" ");  
+        int count=0;
+            for(String i: parts)        
+           {                            
+             count++;  
+           }
+            System.out.println("Number of words in given sentence are: "+count);
+	}
+}
+/*-------------------------------------------------------------------------------
+output:
+Enter your sentence:
+
+this is a joke, right?
+
+Number of words in given sentence are: 5
