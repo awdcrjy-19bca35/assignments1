72bfd82da84acca818d871feee7a1318767839e1
awdcrjy-19bca35 <86473369+awdcrjy-19bca35@users.noreply.github.com>
2021-07-13 22:47:48 +0530

    Update fact13.java

--- fact13.java
+++ fact13.java
@@ -1,26 +1,26 @@
-// java program to print factorialof given number using nested methods // date: 13 july 2021
+// java program to print factorial of given number using nested methods // date: 13 july 2021
 import java.util.*;
 public class Main
 { 
      public static void display(int n)  // display method
-     	{
-	      System.out.println("Factorial of given number is: ");
-	      fact(n);          // method call for fact method
-	    }
-	     public static void fact(int n) // fact method
-	    { int fact=1;
+     {
+	 System.out.println("Factorial of given number is: ");
+	 fact(n);                // method call for fact method
+     }
+     public static void fact(int n) // fact method
+     { int fact=1;
      	for(int i=1;i<=n;i++)  
     	{
 	      fact=fact*i;
     	}
 	    System.out.print(fact);
-	    }
+     }
   public static void main(String[] args) {
 	Scanner p=new Scanner (System.in);
 	System.out.println("Enter the number:");
 	int n=p.nextInt();
-  display(n); // invoking display method (method call)
-}
+        display(n); // invoking display method (method call)
+  }
 }
 /*-------------------------------------------------------------------------------------
 output:
